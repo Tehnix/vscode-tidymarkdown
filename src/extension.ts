679c3718d630eca648a7c9062150f54963dc85b4
HitkoDev <development@hitko.si>
2020-03-22 20:30:08 +0100

    Add option to disable `ensureFirstHeaderIsH1`

--- src/extension.ts
+++ src/extension.ts
@@ -4,19 +4,21 @@ import * as vscode from 'vscode';
 const tidyMarkdown: any = require('tidy-markdown');
 
 
-function getRangeOfDocument(document: vscode.TextDocument) : vscode.Range {
+function getRangeOfDocument(document: vscode.TextDocument): vscode.Range {
     const start = new vscode.Position(0, 0);
     const end = new vscode.Position(document.lineCount - 1, document.lineAt(document.lineCount - 1).text.length);
     return new vscode.Range(start, end);
 }
 
-function _runTidyMarkdown(document: vscode.TextDocument, range: vscode.Range) : string {
+function _runTidyMarkdown(document: vscode.TextDocument, range: vscode.Range, options: vscode.WorkspaceConfiguration): string {
     var content = document.getText(range);
-    return tidyMarkdown(content);
+    return tidyMarkdown(content, {
+        ensureFirstHeaderIsH1: options.get('ensureFirstHeaderIsH1', true)
+    });
 }
 
-function runTidyMarkdown(document: vscode.TextDocument, range: vscode.Range) : vscode.TextEdit[] {
-    var formattedText : string = _runTidyMarkdown(document, range);
+function runTidyMarkdown(document: vscode.TextDocument, range: vscode.Range, options: vscode.WorkspaceConfiguration): vscode.TextEdit[] {
+    var formattedText: string = _runTidyMarkdown(document, range, options);
     return [new vscode.TextEdit(range, formattedText)];
 }
 
@@ -29,7 +31,7 @@ export function activate(context: vscode.ExtensionContext) {
             var tidyMarkdownConfig = vscode.workspace.getConfiguration('tidyMarkdown')
             if (tidyMarkdownConfig && !tidyMarkdownConfig['disableFormatter']) {
                 const range = getRangeOfDocument(document);
-                const formattedText : vscode.TextEdit[] = runTidyMarkdown(document, range);
+                const formattedText: vscode.TextEdit[] = runTidyMarkdown(document, range, tidyMarkdownConfig);
                 return formattedText;
             }
             return [];
@@ -41,7 +43,7 @@ export function activate(context: vscode.ExtensionContext) {
         provideDocumentRangeFormattingEdits(document: vscode.TextDocument, range: vscode.Range, options: vscode.FormattingOptions, token: vscode.CancellationToken): vscode.TextEdit[] {
             var tidyMarkdownConfig = vscode.workspace.getConfiguration('tidyMarkdown')
             if (tidyMarkdownConfig && !tidyMarkdownConfig['disableFormatter']) {
-                const formattedText : vscode.TextEdit[] = runTidyMarkdown(document, range);
+                const formattedText: vscode.TextEdit[] = runTidyMarkdown(document, range, tidyMarkdownConfig);
                 return formattedText;
             }
             return [];
